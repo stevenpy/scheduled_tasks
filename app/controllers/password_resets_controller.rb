5f2288c8d7e67afc003be541196a5bfe0e9c54ff
stevenpy <stevenphilbert@gmail.com>
2024-08-14 17:35:32 +0200

    Add PasswordMailer to send reset password email to user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,11 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      PasswordMailer.with(user: @user).reset.deliver_now
+    else
+    end
+    redirect_to root_path, notice: "If an account was created with this email, we have sent a link to reset your password"
   end
 end
