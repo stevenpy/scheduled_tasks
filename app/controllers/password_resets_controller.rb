f8c66df6c32dfb9711e5dda34a819a2a7e93ac8e
stevenpy <stevenphilbert@gmail.com>
2024-08-14 18:56:30 +0200

    Generate form with token to reset password, work

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,8 +6,28 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user.present?
       PasswordMailer.with(user: @user).reset.deliver_now
-    else
     end
     redirect_to root_path, notice: "If an account was created with this email, we have sent a link to reset your password"
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+      redirect_to sign_in_path, notice: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password has been updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
