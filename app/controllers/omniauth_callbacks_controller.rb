e0f8ee6e135b1e844ae29ae3a20cadce75022e2c
stevenpy <stevenphilbert@gmail.com>
2024-08-16 17:45:39 +0200

    Edit twitter call method to init and add validations on account model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,8 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    Current.user.twitter_accounts.create(
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
       name: auth.info.name,
-      username: auth.info.nickname,
       image: auth.info.image,
       token: auth.credentials.token,
       secret: auth.credentials.secret,
