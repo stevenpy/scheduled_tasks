e1217fa8453f63aa077b3fdd453e8b5b308e7d7a
stevenpy <stevenphilbert@gmail.com>
2024-08-19 20:54:23 +0200

    edit find_by method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   def set_current_user
     if session[:user_id]
-      Current.user = User.find_by_id(session[:user_id])
+      Current.user = User.find_by(id: session[:user_id])
     end
   end
 
