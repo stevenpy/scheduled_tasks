ff6570288a9a60d4d4f764f596961bdd131dca76
stevenpy <stevenphilbert@gmail.com>
2024-08-20 13:51:33 +0200

    Add edit and destroy method for Tweet and associates views and partials

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: [:show, :update, :destroy, :edit]
 
   def index
     @tweets = Current.user.tweets
@@ -18,9 +19,29 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was successfully unscheduled"
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
