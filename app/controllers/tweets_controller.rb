09d73e2b68cebd713aeba567131b54a3b453713b
stevenpy <stevenphilbert@gmail.com>
2024-08-20 14:43:51 +0200

    Add twitter gem and client method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
-  before_action :set_tweet, only: [:show, :update, :destroy, :edit]
+  before_action :set_tweet, only: [:update, :destroy, :edit]
 
   def index
     @tweets = Current.user.tweets
