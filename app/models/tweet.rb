f729d8a748efb53442c0a9110b916d04d7f844d2
stevenpy <stevenphilbert@gmail.com>
2024-08-20 15:29:43 +0200

    Add job to publish tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,11 +9,17 @@ class Tweet < ApplicationRecord
     self.publish_at ||= 24.hours.from_now
   end
 
+  after_save_commit do
+    if publish_at_previously_changed?
+      TweetJob.set(wait_until: publish_at).perform_later(self)
+    end
+  end
+
   def puslished?
     tweet_id?
   end
 
-  def pusblish_to_twitter!
+  def publish_to_twitter!
     tweet = twitter_account.client.update(body)
     update(tweet_id: tweet.id)
   end
